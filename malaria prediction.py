d72a23563f0eb12d61549002269e12ab3487be22
Elikanah13 <elikanahodionyi@gmail.com>
2026-04-05 11:32:57 +0300

    Update malaria prediction.py

--- malaria prediction.py
+++ malaria prediction.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyN8asvCd1cMjmYfc/jzDM42",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
